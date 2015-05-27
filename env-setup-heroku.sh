c17f7dad157090af2aa4080c8cb233aa5f624c22
james sangho nah <sangho.nah@gmail.com>
2015-05-28 01:03:08 +1200

    env for cache password

--- env-setup-heroku.sh
+++ env-setup-heroku.sh
@@ -6,6 +6,7 @@
 
 heroku config:set CACHE_HOST=
 heroku config:set CACHE_PORT=
+heroku config:set CACHE_PASSWORD=
 heroku config:set TWITTER_APP_CONSUMER_KEY=
 heroku config:set TWITTER_APP_CONSUMER_SECRET=
 heroku config:set TWITTER_USER_ACCESS_TOKEN=
